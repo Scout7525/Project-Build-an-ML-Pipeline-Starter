1fda710210b08bb3e99c96b3c070b000bf83aa22
Scout7525 <scout7525@gmail.com>
2025-04-12 17:09:18 -0400

    Clean up basic_cleaning step

--- main.py
+++ main.py
@@ -51,22 +51,18 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            # Preprocess data coloumns
-            _ = mlflow.run(
-                os.path.join(
-                   root_path,
-                   "src",
-                   "basic_cleaning"),
-                "main",
-                parameters={
-                    "input_artifact": "sample.csv:latest",
-                    "output_artifact": "clean_sample.csv",
-                    "output_type": "clean_sample",
-                    "output_description": "Data with outliers and null values removed",
-                    "min_price": config['etl']['min_price'],
-                    "max_price": config['etl']['max_price']},
-            )
-            
+            mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+            entry_point="main",
+            parameters={
+                "input_artifact": "sample.csv:latest",  
+                "output_artifact": "clean_sample.csv",
+                "output_type": "cleaned_data",
+                "output_description": "Cleaned dataset after basic preprocessing",
+                "min_price": config["etl"]["min_price"],
+                "max_price": config["etl"]["max_price"],
+            },
+        )   
 
         if "data_check" in active_steps:
             ##################
